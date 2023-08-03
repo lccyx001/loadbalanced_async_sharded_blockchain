621fc763cbb65b6593ce4ee80f9b622b4ffd81de
lccyx001 <lccyx002@163.com>
2023-08-03 10:23:34 +0800

    init repo

---
+++ lasb/http_server/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, )
+    
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+
+app = create_app()
---
+++ lasb/main.py
@@ -0,0 +1,5 @@
+from http_server import app
+
+if __name__ == "__main__":
+    app.run(port=2020,host="127.0.0.1",debug=True)
+    
