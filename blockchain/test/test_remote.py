e6cade751143cffb280f9711c98de45255d146f7
lccyx001 <lccyx002@163.com>
2024-03-26 11:31:58 +0000

    update test remote

--- blockchain/test/test_remote.py
+++ blockchain/test/test_remote.py
@@ -4,7 +4,7 @@ sys.path.append(r'../../..')
 from loadbalanced_async_sharded_blockchain.common.rpcbase import RPCBase
 
 server_host = "172.19.18.8"
-local_host = "127.0.0.1"
+local_host = "0.0.0.0"
 port = "2000"
 
 
@@ -18,6 +18,7 @@ def run_client():
     rpc = RPCBase(broadcast_channels,local_host,port)
     rpc.connect_broadcast_channel()
     client = rpc.remote_channels.get(0)[1]
+    print(client)
     print("connect success",client.echo())
 
 if __name__ == "__main__":
