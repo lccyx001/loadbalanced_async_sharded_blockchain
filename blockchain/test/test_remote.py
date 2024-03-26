4953724c653d45c79770a81b33ada21553991a76
lccyx001 <lccyx002@163.com>
2024-03-26 11:24:07 +0000

    update test remote

--- blockchain/test/test_remote.py
+++ blockchain/test/test_remote.py
@@ -4,20 +4,21 @@ sys.path.append(r'../../..')
 from loadbalanced_async_sharded_blockchain.common.rpcbase import RPCBase
 
 server_host = "172.19.18.8"
-server_port = "2001"
-client_host = "127.0.0.1"
-client_port = "2101"
+local_host = "127.0.0.1"
+port = "2000"
+
 
 def run_server():
-    broadcast_channels = [[1,"tcp://" + client_host + ":" + client_port ]]
-    rpc = RPCBase(broadcast_channels,server_host,server_port)
+    broadcast_channels = []
+    rpc = RPCBase(broadcast_channels,local_host,port)
     rpc.run_forever()
 
 def run_client():
-    broadcast_channels =[[0,"tcp://" + server_host + ":" + server_port]]
-    rpc = RPCBase(broadcast_channels,client_host,client_port)
+    broadcast_channels =[[0,"tcp://" + server_host + ":" + port]]
+    rpc = RPCBase(broadcast_channels,local_host,port)
     rpc.connect_broadcast_channel()
-    print("connect success")
+    client = rpc.remote_channels.get(0)[1]
+    print("connect success",client.echo())
 
 if __name__ == "__main__":
     if sys.argv[1] == "s":
